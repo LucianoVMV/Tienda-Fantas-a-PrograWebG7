69e61f40d5c6955dbb984e8c2fc4942d4318feff
victorgp-git <vguimaray@gmail.com>
2025-10-07 22:51:34 -0500

    Delete src/pages/carrito.tsx

--- src/pages/carrito.tsx
+++
@@ -1,8 +0,0 @@
-export default function Cart() {
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>Carrito de Compras</h1>
-      <p>Aquí se mostrarán los productos seleccionados</p>
-    </div>
-  );
-}
